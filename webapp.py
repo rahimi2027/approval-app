6f21d901fc255e88e8367e1c38bbb9facbf7e261
rahimi2027 <wais.rahimi86@gmail.com>
2026-09-05 12:06:47 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -210,16 +210,6 @@ st.set_page_config(
 )
 
 # ============================================================
-# ✅ HIDE TOP-RIGHT 3-DOTS MENU
-# ============================================================
-hide_menu_style = """
-<style>
-#MainMenu {visibility: hidden;}
-header {visibility: hidden;}
-</style>
-"""
-st.markdown(hide_menu_style, unsafe_allow_html=True)
-# ============================================================
 # FILE PATHS
 # ============================================================
 import sys
