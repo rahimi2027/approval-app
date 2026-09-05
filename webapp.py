c12d50eefab5c29bb17617d9ef5fca5f00c011a6
rahimi2027 <wais.rahimi86@gmail.com>
2026-09-06 00:27:59 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -928,7 +928,7 @@ else:
     # ========================================================
     # MANAGER PORTAL
     # ========================================================
-    elif user["role"] == "Manager":
+    elif user["role"] in ["Manager", "Staff","Team Member"]:
         if st.session_state.editing_request_id:
             eid = st.session_state.editing_request_id
             rec = next((r for r in all_live_requests if int(r["id"]) == int(eid)), None)
