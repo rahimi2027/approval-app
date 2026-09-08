2b58d6681065ec9e73d878486980167969f9ff09
rahimi2027 <wais.rahimi86@gmail.com>
2026-09-08 21:02:29 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -76,7 +76,7 @@ USER_DB_PATH = os.path.join(APP_FOLDER, "user_database.xlsx")
 SETTINGS_PATH = os.path.join(APP_FOLDER, "settings.xlsx")
 
 # ─── GOOGLE DRIVE ───
-GOOGLE_DRIVE_FOLDER_ID = "1YxWsEYbkYdEh09q7LNXJgezC7dU7PRXk"
+GOOGLE_DRIVE_FOLDER_ID = "1oecpaa8c5tryCtcIAnbjEXemGDonvgPZ"
 
 # ─── ONEDRIVE / MICROSOFT GRAPH ───
 ONEDRIVE_CLIENT_ID = ""         # ← Fill later when ready
